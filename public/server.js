44335e74b1fa438172f8416ea920d72efa66293f
PauloSBL <114448029+PauloSBL@users.noreply.github.com>
2023-10-25 10:50:53 -0300

    Add files via upload

---
+++ public/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const path = require('path');
+const cors = require('cors');
+
+const io = require('socket.io')(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(3000,()=>{
+    console.log('Configurando server Express...')
+})
+io.listen(5000)
+
+io.on('connection', (socket)=>{
+    console.log('conectado')
+
+    socket.on('message', (data)=>{
+        io.emit('message', data)
+    })
+
+})
+
+io.on('disconnect', (data)=>{
+    console.log('desconectado')
+})
+
